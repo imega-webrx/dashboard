ffc5f3d118130ef1e4f8ef02af30e48f70536987
BlackWood99 <RenatBW@yandex.ru>
2021-12-04 20:46:15 +0300

    test

--- server/index.js
+++ server/index.js
@@ -14,22 +14,23 @@ const app = express();
 
 app.get('/', (req, res) => {
   // const app = ReactDOMServer.renderToString(<App />);
-  const app = ReactDOMServer.renderToString(<div><h1>Dashboard</h1></div>);
+  // const app = ReactDOMServer.renderToString(<div><h1>Dashboard</h1></div>);
 
-  const indexFile = path.resolve('./build/index.html');
-  fs.readFile(indexFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Something went wrong:', err);
-      return res.status(500).send('Oops, better luck next time!');
-    }
+  // const indexFile = path.resolve('./build/index.html');
+  // fs.readFile(indexFile, 'utf8', (err, data) => {
+  //   if (err) {
+  //     console.error('Something went wrong:', err);
+  //     return res.status(500).send('Oops, better luck next time!');
+  //   }
 
-    return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-    );
-  });
+  //   return res.send(
+  //     data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+  //   );
+  // });
+  res.send('Hello World!');
 });
 
-app.use(express.static('./build'));
+// app.use(express.static('./build'));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
