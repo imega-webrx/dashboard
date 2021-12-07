62376e2a89822ad9ab7fd1e5563279083590ef69
BlackWood99 <78879606+BlackWood99@users.noreply.github.com>
2021-12-07 13:35:57 +0300

    changed src/index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-// import App from "./App";
+import path from 'path';
+import fs from 'fs';
 
-ReactDOM.render(<div><h1>Hello world!</h1></div>, document.getElementById("root"));
+import React from 'react';
+import express from 'express';
+import ReactDOMServer from 'react-dom/server';
+
+// import App from '../src/App';
+
+const PORT = process.env.PORT || 3006;
+const app = express();
+
+// ...
+
+app.get('/', (req, res) => {
+  // const app = ReactDOMServer.renderToString(<App />);
+  // const app = ReactDOMServer.renderToString(<div><h1>Dashboard</h1></div>);
+
+  // const indexFile = path.resolve('./build/index.html');
+  // fs.readFile(indexFile, 'utf8', (err, data) => {
+  //   if (err) {
+  //     console.error('Something went wrong:', err);
+  //     return res.status(500).send('Oops, better luck next time!');
+  //   }
+
+  //   return res.send(
+  //     data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+  //   );
+  // });
+  res.send('Hello World!');
+});
+
+// app.use(express.static('./build'));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
--- webpack.server.js
+++ webpack.server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: "./server/index.js",
+  entry: "./src/index.js",
 
   target: "node",
 
