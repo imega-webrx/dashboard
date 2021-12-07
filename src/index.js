a9de60a65954a3c888d8b6bc815bf3e870455cdf
BlackWood99 <RenatBW@yandex.ru>
2021-12-07 20:13:29 +0300

    test1.0

--- src/index.js
+++ src/index.js
@@ -5,32 +5,32 @@ import React from 'react';
 import express from 'express';
 import ReactDOMServer from 'react-dom/server';
 
-// import App from '../src/App';
+import App from '../src/App';
 
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.DASHBOARD_PORT || 9000;
 const app = express();
 
 // ...
 
 app.get('/', (req, res) => {
   // const app = ReactDOMServer.renderToString(<App />);
-  // const app = ReactDOMServer.renderToString(<div><h1>Dashboard</h1></div>);
+  const appTemplate = ReactDOMServer.renderToString(<div><h1>Dashboard</h1></div>);
 
-  // const indexFile = path.resolve('./build/index.html');
-  // fs.readFile(indexFile, 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.error('Something went wrong:', err);
-  //     return res.status(500).send('Oops, better luck next time!');
-  //   }
+  const indexFile = path.resolve('./build/index.html');
+  fs.readFile(indexFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
+    }
 
-  //   return res.send(
-  //     data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-  //   );
-  // });
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${appTemplate}</div>`)
+    );
+  });
   res.send('Hello World!');
 });
 
-// app.use(express.static('./build'));
+app.use(express.static('./build'));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
