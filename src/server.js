f800ac97dbba89fe76352118517d3e5b96133a8b
Dmitry Stoletov <irvis@imega.ru>
2021-12-11 13:45:48 +0300

    fix build server
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

---
+++ src/server.js
@@ -0,0 +1,57 @@
+// import path from "path";
+// import fs from "fs";
+
+import React from "react";
+import express from "express";
+import ReactDOMServer from "react-dom/server";
+
+// import App from "../src/App";
+
+const PORT = process.env.DASHBOARD_PORT || 9000;
+const app = express();
+
+// ...
+
+const tmpl = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Dashboard</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+  </body>
+</html>`;
+
+app.get("/", (req, res) => {
+    // const app = ReactDOMServer.renderToString(<App />);
+    const appTemplate = ReactDOMServer.renderToString(
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    );
+
+    //   const indexFile = path.resolve('./build/index.html');
+    //   fs.readFile(indexFile, 'utf8', (err, data) => {
+    //     if (err) {
+    //       console.error('Something went wrong:', err);
+    //       return res.status(500).send('Oops, better luck next time!');
+    //     }
+
+    return res.send(
+        tmpl.replace(
+            '<div id="root"></div>',
+            `<div id="root">${appTemplate}</div>`
+        )
+    );
+    //   });
+    // res.send("Hello World!");
+});
+
+// app.use(express.static("./build"));
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
