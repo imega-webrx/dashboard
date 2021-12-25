e641a39a79eaa73de5c5a022f17457e92304b809
Dmitry Stoletov <irvis@imega.ru>
2021-12-25 21:53:13 +0300

    get catalog
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export async function getServerSideProps() {
                 }
             `,
             variables: {
-                in: [rootFolder],
+                id: rootFolder,
             },
             errorPolicy: "all",
         })
