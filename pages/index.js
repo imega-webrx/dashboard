61903759cdcf2bc5fe7dda5065514d991583c18e
Dmitry Stoletov <irvis@imega.ru>
2021-12-25 18:12:45 +0300

    add apollo client in ssr mode;
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
+import { gql } from "@apollo/client";
+import client from "../src/apollo-client";
 import { Button } from "antd";
 
 import Layout from "../src/Page/Layout";
 import "../src/index.less";
 
-const MainPage = () => (
+const MainPage = ({ folders }) => (
     <Layout>
         <h1>
-            Main<Button type="primary">aaaa</Button>
+            Main<Button type="primary">{folders[0].title}</Button>
         </h1>
     </Layout>
 );
+
+export async function getServerSideProps() {
+    const { data } = await client.query({
+        query: gql`
+            query GetFolders($in: [ID]!) {
+                getFolders(uuIds: $in) {
+                    id
+                    title
+                    description
+                }
+            }
+        `,
+        variables: {
+            in: ["ed4bf8f5-8b4e-435b-83cc-27feada6136a"],
+        },
+    });
+
+    return {
+        props: {
+            folders: data.getFolders,
+        },
+    };
+}
 
 export default MainPage;
