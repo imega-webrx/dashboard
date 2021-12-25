c6ad969e976eac2784ec8de8bf1f0399d479baf9
Dmitry Stoletov <irvis@imega.ru>
2021-12-25 16:07:37 +0300

    change build dir
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const withAntdLess = require("next-plugin-antd-less");
 
-module.exports = withAntdLess({
-    lessVarsFilePathAppendToEndOfContent: false, // optional
-    cssLoaderOptions: {
-        mode: "local",
-        localIdentName: "[path][name]__[local]--[hash:base64:5]",
-        exportLocalsConvention: "camelCase",
-        exportOnlyLocals: false,
-    },
-    webpack(config) {
-        return config;
-    },
-});
+module.exports = {
+    distDir: "build",
+    ...withAntdLess({
+        lessVarsFilePathAppendToEndOfContent: false, // optional
+        cssLoaderOptions: {
+            mode: "local",
+            localIdentName: "[path][name]__[local]--[hash:base64:5]",
+            exportLocalsConvention: "camelCase",
+            exportOnlyLocals: false,
+        },
+        webpack(config) {
+            return config;
+        },
+    }),
+};
