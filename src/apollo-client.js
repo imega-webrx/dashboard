7a72b59117051901018314f353b967672a1ab8f7
Dmitry Stoletov <irvis@imega.ru>
2021-12-28 17:13:53 +0300

    add ssrMode in apollo client
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -4,6 +4,7 @@ const addr =
     typeof window === "undefined" ? "http://localhost" : "https://webrx.ru";
 
 const client = new ApolloClient({
+    ssrMode: typeof window === "undefined",
     uri: `${addr}/graphql`,
     cache: new InMemoryCache(),
 });
